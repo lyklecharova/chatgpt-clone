38273e839550be5f4647cec8b9c6cc7367916183
lyklecharova <lklecharova9@gmail.com>
2025-01-29 22:36:53 +0200

    feat: add routing with react-router-dom and update structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './routes/homepage/Homepage';
+import DashboardPage from './routes/dashboardPage/DashboardPage';
+import ChatPage from './routes/chatPage/ChatPage/';
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />,
+    },
+    {
+        path: 'dashboard',
+
+        children: [
+            {path: "/dashboard", element: <DashboardPage/>},
+            {path: "/dashboard/:id", element: <ChatPage/>},
+        ],
+    },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
