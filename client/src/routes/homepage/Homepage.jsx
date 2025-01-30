b1e8f201959c8736c27fcd9e271a73988ce05d6a
lyklecharova <lklecharova9@gmail.com>
2025-01-30 23:07:11 +0200

    feat: updated homepage with new design, navigation link, and header and background styles

--- client/src/routes/homepage/Homepage.jsx
+++ client/src/routes/homepage/Homepage.jsx
@@ -1,9 +1,22 @@
-import './homepage.css'
+import { Link } from 'react-router-dom';
+import './homepage.css';
 
 const Homepage = () => {
-  return (
-    <div className='homepage'>Homepage</div>
-  )
-}
+    return (
+        <div className="homepage">
+            <img src="/orbital.png" alt=""  className='orbital'/>
+            <div className="left">
+                <h1>Lama AI</h1>
+                <h2>Supercharge your creativity and productivity</h2>
+                <h3>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Velit facere nostrum totam architecto.
+                </h3>
+                <Link to="/dashboard">Get Started</Link>
+            </div>
+            <div className="right"></div>
+        </div>
+    );
+};
 
-export default Homepage
+export default Homepage;
