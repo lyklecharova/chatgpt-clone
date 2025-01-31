69e026522c58dc78e4d4d01239fe6db2734e249d
lyklecharova <lklecharova9@gmail.com>
2025-01-31 10:43:06 +0200

    feat:  added layouts and authentication routes, updated routing structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,18 +5,40 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './routes/homepage/Homepage';
 import DashboardPage from './routes/dashboardPage/DashboardPage';
 import ChatPage from './routes/chatPage/ChatPage/';
+import RootLayout from './layouts/rootLayout/RootLayout';
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout';
+import SignUpPage from './routes/signUpPage/SignUpPage';
+import SignInPage from './routes/signInPage/SignInPage';
 
 const router = createBrowserRouter([
     {
-        path: '/',
-        element: <HomePage />,
-    },
-    {
-        path: 'dashboard',
-
+        element: <RootLayout />,
         children: [
-            {path: "/dashboard", element: <DashboardPage/>},
-            {path: "/dashboard/:id", element: <ChatPage/>},
+            {
+                path: '/',
+                element: <HomePage />,
+            },
+            {
+                path: '/sign-in/*',
+                element: <SignInPage />,
+            },
+            {
+                path: '/sign-up/*',
+                element: <SignUpPage />,
+            },
+            {
+                element: <DashboardLayout />,
+                children: [
+                    {
+                        path: '/dashboard',
+                        element: <DashboardPage />,
+                    },
+                    {
+                        path: '/dashboard/chats/:id',
+                        element: <ChatPage />,
+                    },
+                ],
+            },
         ],
     },
 ]);
