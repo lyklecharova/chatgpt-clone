a37aa3900f6eb636c2471b46b8786cc64c64fc94
lyklecharova <lklecharova9@gmail.com>
2025-02-01 10:11:00 +0200

    feat: add authentication redirect to DashboardPage

--- client/src/routes/dashboardPage/DashboardPage.jsx
+++ client/src/routes/dashboardPage/DashboardPage.jsx
@@ -1,9 +1,21 @@
-import './dashboardPage.css'
+import { useAuth } from '@clerk/clerk-react';
+import './dashboardPage.css';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const DashboardPage = () => {
-  return (
-    <div className='dashboardPage'>DashboardPage</div>
-  )
-}
+    const { userId, isLoaded } = useAuth();
 
-export default DashboardPage
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (isLoaded && !userId) {
+            navigate('/sign-in');
+        }
+    }, [isLoaded, userId, navigate]);
+
+    if (!isLoaded) return 'Loading...';
+    
+    return <div className="dashboardPage">DashboardPage</div>;
+};
+
+export default DashboardPage;
