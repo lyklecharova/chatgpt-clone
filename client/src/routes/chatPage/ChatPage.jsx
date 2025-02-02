192c16322e254077906bb5f963f7d283759a6b34
lyklecharova <lklecharova9@gmail.com>
2025-02-02 21:42:35 +0200

    feat: Add NewPrompt to ChatPage with styled AI and user messages.

--- client/src/routes/chatPage/ChatPage.jsx
+++ client/src/routes/chatPage/ChatPage.jsx
@@ -1,9 +1,41 @@
-import './chatPage.css'
+import './chatPage.css';
+import NewPrompt from '../../components/newPrompt/NewPrompt';
 
 const ChatPage = () => {
-  return (
-    <div className='chatPage'>ChatPage</div>
-  )
-}
+    return (
+        <div className="chatPage"> 
+            <div className="wrapper">
+                <div className="chat">
+                    <div className="message">Test message from AI</div>
+                    <div className="message user">
+                        Test message from user Lorem, ipsum dolor sit amet
+                        consectetur adipisicing elit. Consequuntur rerum quo
+                        mollitia incidunt, iste beatae aliquam atque autem velit
+                        itaque temporibus perferendis. Eos suscipit,
+                        voluptatibus laborum aliquid veritatis sit culpa?
+                    </div>
+                    <div className="message ">Test message from AI</div>
+                    <div className="message user">Test message from user</div>
 
-export default ChatPage
+                    <div className="message">Test message from AI</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message ">Test message from AI</div>
+                    <div className="message user">Test message from user</div>
+
+                    <div className="message">Test message from AI</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message ">Test message from AI</div>
+                    <div className="message user">Test message from user</div>
+
+                    <div className="message">Test message from AI</div>
+                    <div className="message user">Test message from user</div>
+                    <div className="message ">Test message from AI</div>
+                    <div className="message user">Test message from user</div>
+                    <NewPrompt/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ChatPage;
