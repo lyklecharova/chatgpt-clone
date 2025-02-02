9a9d5033f7673b02cbd30c8644640bfda9e0c8fc
lyklecharova <lklecharova9@gmail.com>
2025-02-02 10:24:39 +0200

    feat: Update DashboardPage with UI enhancements and authentication check

--- client/src/routes/dashboardPage/DashboardPage.jsx
+++ client/src/routes/dashboardPage/DashboardPage.jsx
@@ -1,21 +1,38 @@
-import { useAuth } from '@clerk/clerk-react';
 import './dashboardPage.css';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const DashboardPage = () => {
-    const { userId, isLoaded } = useAuth();
-
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (isLoaded && !userId) {
-            navigate('/sign-in');
-        }
-    }, [isLoaded, userId, navigate]);
-
-    if (!isLoaded) return 'Loading...';
-    
-    return <div className="dashboardPage">DashboardPage</div>;
+    return (
+        <div className="dashboardPage">
+            <div className="texts">
+                <div className="logo">
+                    <img src="/logo.png" alt="" />
+                    <h1>Lama AI</h1>
+                </div>
+                <div className="options">
+                    <div className="option">
+                        <img src="/chat.png" alt="" />
+                        <span>Create a New Chat</span>
+                    </div>
+                    <div className="option">
+                        <img src="/image.png" alt="" />
+                        <span>Analyze Images</span>
+                    </div>
+                    <div className="option">
+                        <img src="/code.png" alt="" />
+                        <span>Help me with my Code</span>
+                    </div>
+                </div>
+            </div>
+            <div className="formContainer">
+                <form>
+                    <input type="text" placeholder='Ask me anything...' />
+                    <button>
+                        <img src="./arrow.png" alt="" />
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default DashboardPage;
