3d68aeccf04dff808c1ff08eccdccd84b035f073
lyklecharova <lklecharova9@gmail.com>
2025-02-03 17:22:36 +0200

    feat: Add chat creation API and user-chats handling with MongoDB integration

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import ImageKit from 'imagekit';
-
+import mongoose from 'mongoose';
+import UserChats from './models/userChats.js';
+import Chat from './models/chat.js';
 
 const port = process.env.PORT || 3000;
 const app = express();
+
 app.use(express.json());
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
 }));
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.log(err);
+
+    }
+}
 
 const imagekit = new ImageKit({
     urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
@@ -20,8 +33,56 @@ app.get("/api/upload", (req, res) => {
     const result = imagekit.getAuthenticationParameters();
     res.send(result);
 
+});
+
+app.post("/api/chats", async (req, res) => {
+    const { userId, text } = req.body;
+
+    try {
+        // create a new a chat
+        const newChat = new Chat({
+            userId: userId,
+            history: [{ role: "user", parts: [{ text }] }]
+        });
+
+        const savedChat = await newChat.save();
+
+        // check if the  userchats exist
+        const userChats = await UserChats.find({ userId: userId });
+
+        // IF doesn't exist create a new one and add the chat in the chats array
+        if (!userChats.length) {
+            const newUserChats = new UserChats({
+                userId: userId,
+                chats: [
+                    {
+                        _id: savedChat._id,
+                        title: text.substring(0, 40),
+                    },
+                ]
+            });
+            await newUserChats.save();
+        } else {
+            // If exists, push the chat to the existing array
+            await UserChats.updateOne({ userId: userId }, {
+                $push: {
+                    chats: {
+                        _id: savedChat._id,
+                        title: text.substring(0, 40),
+                    }
+                }
+            });
+
+            res.status(201).send(newChat._id);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error creating chat!");
+    }
+
 })
 app.listen(port, () => {
+    connect();
     console.log(`Server is running on ${port}`);
 
 });
