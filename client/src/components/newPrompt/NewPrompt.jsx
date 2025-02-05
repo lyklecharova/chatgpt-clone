dbdc2d76c20b61b09435b4635fe44be363c1bcbe
lyklecharova <lklecharova9@gmail.com>
2025-02-05 21:03:15 +0200

    feat: Refactored NewPrompt component: Added React Query mutation, improved state management, and optimized initial message handling

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -4,7 +4,8 @@ import Upload from '../upload/Upload';
 import { IKImage } from 'imagekitio-react';
 import model from '../../lib/gemini';
 import Markdown from 'react-markdown';
-const NewPrompt = () => {
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+const NewPrompt = ({ data }) => {
     const [question, setQuestion] = useState('');
     const [answer, setAnswer] = useState('');
 
@@ -34,38 +35,91 @@ const NewPrompt = () => {
     });
 
     const endRef = useRef(null);
+    const formRef = useRef(null);
 
     useEffect(() => {
         endRef.current.scrollIntoView({ behavior: 'smooth' });
     }, [question, answer, img.dbData]);
 
-    const add = async (text) => {
-        setQuestion(text);
-        const result = await chat.sendMessageStream(
-            Object.entries(img.aiData).length ? [img.aiData, text] : [text]
-        );
-        let accumulatedText = '';
-        for await (const chunk of result.stream) {
-            const chunkText = chunk.text();
-            console.log(chunkText);
-            accumulatedText += chunkText;
-            setAnswer(accumulatedText);
-        }
+    const queryClient = useQueryClient();
 
-        setImg({
-            isLoading: false,
-            error: '',
-            dbData: {},
-            aiData: {},
-        });
+    const mutation = useMutation({
+        mutationFn: () => {
+            return fetch(
+                `${import.meta.env.VITE_API_URL}/api/chats/${data._id}`,
+                {
+                    method: 'PUT',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        question: question.length ? question : undefined,
+                        answer,
+                        img: img.dbData?.filePath || undefined,
+                    }),
+                }
+            ).then((res) => res.json());
+        },
+        onSuccess: () => {
+            queryClient
+                .invalidateQueries({ queryKey: ['chat', data._id] })
+                .then(() => {
+                    formRef.current.reset();
+                    setQuestion('');
+                    setAnswer('');
+                    setImg({
+                        isLoading: false,
+                        error: '',
+                        dbData: {},
+                        aiData: {},
+                    });
+                });
+        },
+        onError: (err) => {
+            console.log(err);
+        },
+    });
+
+    const add = async (text, isInitial) => {
+        if (!isInitial) setQuestion(text);
+
+        try {
+            const result = await chat.sendMessageStream(
+                Object.entries(img.aiData).length ? [img.aiData, text] : [text]
+            );
+            let accumulatedText = '';
+            for await (const chunk of result.stream) {
+                const chunkText = chunk.text();
+                console.log(chunkText);
+                accumulatedText += chunkText;
+                setAnswer(accumulatedText);
+            }
+            mutation.mutate();
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const text = e.target.text.value;
         if (!text) return;
-        add(text);
+        add(text, false);
     };
+    // In production we don't need it
+    const hasRun = useRef(false);
+    useEffect(() => {
+        if (!hasRun.current) {
+            if (data?.history?.length === 1) {
+                add(data.history[0].parts[0].text, true);
+            }
+            if (data?.history?.length === 1) {
+                add(data.history[0].parts[0].text, true);
+            }
+        }
+        hasRun.current = true;
+    }, []);
     return (
         <>
             {/* ADD NEW CHAT */}
@@ -85,7 +139,7 @@ const NewPrompt = () => {
                 </div>
             )}
             <div className="endChat" ref={endRef}></div>
-            <form className="newForm" onSubmit={handleSubmit}>
+            <form className="newForm" onSubmit={handleSubmit} ref={formRef}>
                 <Upload setImg={setImg} />
                 <input id="file" type="file" multiple={false} hidden />
                 <input type="text" name="text" placeholder="Ask anything..." />
