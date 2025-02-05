cb90be979ef904310a735bbd85649e6b7f7995c6
lyklecharova <lklecharova9@gmail.com>
2025-02-05 20:19:42 +0200

    feat: Refactor ChatList to fetch user chats dynamically using React Query
    - Integrated @tanstack/react-query to fetch user chats from the backend dynamically.
    - Replaced static chat links with dynamically loaded chat titles.
    - Added a loading state to indicate data fetching progress.
    - Implemented error handling to display a message if fetching fails.
    - Ensured the chat list updates automatically when data changes.

--- client/src/components/chatList/ChatList.jsx
+++ client/src/components/chatList/ChatList.jsx
@@ -1,7 +1,20 @@
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import './chatList.css';
 
 function ChatList() {
+    const { isPending, error, data } = useQuery({
+        queryKey: ['userChats'],
+        queryFn: async () => {
+            const res = await fetch(
+                `${import.meta.env.VITE_API_URL}/api/userchats`,
+                {
+                    credentials: 'include',
+                }
+            );
+            return res.json();
+        },
+    });
     return (
         <div className="chatList">
             <span className="title">DASHBOARD</span>
@@ -11,15 +24,18 @@ function ChatList() {
             <hr />
             <span className="title">RECENT CHAT</span>
             <div className="list">
-                <Link to="/">My chat title</Link>
-                <Link to="/">My chat title</Link>
-                <Link to="/">My chat title</Link>
-                <Link to="/">My chat title</Link>
-                <Link to="/">My chat title</Link>
-                <Link to="/">My chat title</Link>
-                <Link to="/">My chat title</Link>
-                <Link to="/">My chat title</Link>
-       
+                {isPending
+                    ? 'Loading...'
+                    : error
+                    ? 'Something went wrong!'
+                    : data?.map((chat) => (
+                          <Link
+                              to={`/dashboard/chats/${chat._id}`}
+                              key={chat._id}
+                          >
+                              {chat.title}
+                          </Link>
+                      ))}
             </div>
             <hr />
             <div className="upgrade">
